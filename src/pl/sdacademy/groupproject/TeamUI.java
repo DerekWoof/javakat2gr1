a46f3516b4eb8c38a7e3966efac536c5caa179b9
Your Name <you@example.com>
2017-07-13 19:34:45 +0200

    Dodano klasę FootballPlayer

---
+++ src/pl/sdacademy/groupproject/TeamUI.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.groupproject;
+
+
+import java.util.Scanner;
+
+public class TeamUI {
+    public TeamUI getTeam() {
+        return team;
+    }
+
+    public TeamUI( Scanner scanner, TeamUI team ) {
+        this.scanner = scanner;
+        this.team = team;
+    }
+
+    private Scanner scanner;
+    private TeamUI team;
+
+}
