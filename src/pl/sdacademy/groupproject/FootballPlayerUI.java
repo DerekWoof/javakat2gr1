c41d828e49acfe5e3cb315bdf1efe2660cce0574
Maladie <mariusz.maks.szymanski.com>
2017-07-13 19:44:21 +0200

    Utworzono metodę run w FootballPlayerUI

--- src/pl/sdacademy/groupproject/FootballPlayerUI.java
+++ src/pl/sdacademy/groupproject/FootballPlayerUI.java
@@ -1,10 +1,9 @@
 package pl.sdacademy.groupproject;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Scanner;
 
-/**
- * Created by RENT on 2017-07-13.
- */
 public class FootballPlayerUI {
 
     private Scanner scanner;
@@ -17,5 +16,34 @@ public class FootballPlayerUI {
 
     public FootballPlayer getPlayer() {
         return player;
+    }
+
+    public void run(){
+        Scanner read = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Wybierz co chcesz zrobić: wypisz dane pilkarza (wypisz), " +
+                    "edycja imienia (imie), edycja nazwiska (nazwisko), edycja strzelania (strzal), edycja szybkosci (szybkosc), " +
+                    "koniec (koniec)");
+            switch(read.nextLine().toLowerCase()){
+                case "wypisz":
+                    break;
+                case "imie":
+                    break;
+                case "nazwisko":
+                    break;
+                case "strzal":
+                    break;
+                case "szybkosc":
+                    break;
+                case "koniec":
+                    System.out.println("Zakończono edycję");
+                    return;
+                default:
+                    System.out.println("Błedny wybór. Spróbuj ponownie");
+                    System.out.println("");
+            }
+        }
+
     }
 }
