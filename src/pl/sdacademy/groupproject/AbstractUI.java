9f28e19b1874bf0b66340f1334d04dd9445c5c16
Your Name <you@example.com>
2017-07-13 20:50:38 +0200

    Do sprawdzenia 16 i omówienia jak można to połączyć.

---
+++ src/pl/sdacademy/groupproject/AbstractUI.java
@@ -0,0 +1,37 @@
+package pl.sdacademy.groupproject;
+
+
+public abstract class AbstractUI {
+
+//        this.scanner = scanner;
+//        this.player = player;
+//public void run(){
+//
+//    while(true) {
+//        switch(this.readString("Co chcesz zrobić:\n Wypisz dane drużyny (wypisz),\n Edycja nazwy (nazwa),\n Edycja daty zalozenia (datazalozenia)" +
+//                ", \n Dodaj piłkarza do druzyny (dodaj), \n Usuń piłkarza z drużyny (usuń), \n Edytuj piłkarza (edytuj), " +
+//                "\n Koniec (koniec)").toLowerCase()){
+//            case "wypisz":
+//                printTeamInfo();
+//            case "nazwa":
+//                this.setTeamName();
+//                break;
+//            case "datazalozenia":
+//                break;
+//            case "dodaj":
+//                break;
+//            case "usuń":
+//                break;
+//            case "edytuj":
+//                break;
+//            case "koniec":
+//                System.out.println("Zakończono edycję");
+//                return;
+//            default:
+//                System.out.println("Błedny wybór. Spróbuj ponownie");
+//                System.out.println("");
+//        }
+//    }
+//}
+
+}
